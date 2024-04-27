a46f24f62c0dc601f065c41ccbf650cd7c932101
YeetmanLord <60905636+YeetmanLord@users.noreply.github.com>
2024-04-27 18:38:52 -0400

    Error Recreation

---
+++ next-server/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import TestComponent from "ui-library";
+
+export default function Home() {
+  return (
+    <TestComponent/>
+  );
+}
